3477c6cab0fe9aac5c6e5d355c3f2346b0d03ca3
Tom <2470017654@qq.com>
2022-09-01 10:48:52 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  base: 'WebNote',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '组件', link: '/components/' },
+    ],
+    sidebar: {
+      '/components/': [
+        {
+          title: 'Group 1',   // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          children: [
+            ['Table', '表格']
+          ]
+        },
+      ]
+    },
+    lastUpdated: 'Last Updated'
+  }
+}
---
+++ docs/components/Table.md
@@ -0,0 +1,5 @@
+# table
+
+``` javascript
+  console.log('hello')
+```
